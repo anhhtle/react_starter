ee775a8ae0f7ee0b1f8ed9e3cfa4ca76b37a532e
anhhtle <anh.ht.le@gmail.com>
2017-04-18 23:37:49 -0700

    done

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import PersonList from './components/person-list';
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDom.render(<PersonList />, document.getElementById('app'));
+});
 
